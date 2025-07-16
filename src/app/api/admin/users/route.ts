b20594d4c50c250624dd581778552d98f7926583
TheMerchant66 <royw8985@gmail.com>
2025-07-16 15:02:25 -0700

    renamedddddd

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,19 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { isAdmin } from '@/lib/middleware/isAdmin.middleware';
-import { getAllUsersController } from '@/lib/controllers/admin.controller';
-import { dbConnect } from '@/lib/utils/dbConnect.utils';
-import { CustomError } from '@/lib/utils/customError.utils';
+import { NextRequest, NextResponse } from "next/server";
+import { isAdmin } from "@/lib/middleware/isAdmin.middleware";
+import { getAllUsersController } from "@/lib/controllers/admin.controller";
+import { dbConnect } from "@/lib/utils/dbConnect.utils";
+import { CustomError } from "@/lib/utils/customError.utils";
 
 export async function GET(req: NextRequest) {
-    try {
-        await dbConnect();
-        return await isAdmin(async (req) => {
-            return await getAllUsersController();
-        })(req);
-    } catch (error) {
-        if (error instanceof CustomError) {
-            return NextResponse.json({ message: error.message }, { status: error.statusCode });
-        }
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  try {
+    await dbConnect();
+    return await isAdmin(async (req) => {
+      return await getAllUsersController();
+    })(req);
+  } catch (error) {
+    if (error instanceof CustomError) {
+      return NextResponse.json(
+        { message: error.message },
+        { status: error.statusCode }
+      );
     }
+    return NextResponse.json(
+      { message: "An Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
